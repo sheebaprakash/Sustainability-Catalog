e395e1798ce2c485290557e4b7c03d90aa7b4e6a
Sheeba Prakash <94703633+sheebaprakash@users.noreply.github.com>
2025-02-16 21:15:43 -0500

    Update GrandChallenge.tsx

--- src/app/components/GrandChallenge.tsx
+++ src/app/components/GrandChallenge.tsx
@@ -31,7 +31,7 @@ const GrandChallenge: React.FC = () => {
                   </ul>
               <br />
                   Link to 2024 Grand challenge: {' '}
-                  <a href="https://enter.innovationgrandchallenge.ai/2024" className="text-blue-600 hover:underline">https://enter.innovationgrandchallenge.ai/2024</a>.
+                  <a href="https://enter.innovationgrandchallenge.ai/2024" className="text-blue-600 hover:underline">https://enter.innovationgrandchallenge.ai/2024</a>
               
 2024 Grand Challenge Winner - Dr. Letetia Addison of Trinidad and Tobago, for her “AI-Driven Climate Resilience Platform for SIDS.” 
                 
